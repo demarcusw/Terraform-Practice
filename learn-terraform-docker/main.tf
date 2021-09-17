ba40491014f77bae40109c0e0912f46bb714ade4
DeMarcus Williams <demarcusw@users.noreply.github.com>
2021-09-16 21:54:48 -0400

    Updated Docker example. Terraform now deploys 2 docker containers: nginx frontend, NodeJS backend

--- learn-terraform-docker/main.tf
+++ learn-terraform-docker/main.tf
@@ -9,16 +9,53 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+// Build our node app. This will sit behind our NGINX reverse proxy
+resource "docker_image" "node-app" {
+    name = "node-app"
+    build {
+        path = "./node-src/"
+        tag = ["node-app:latest"]
+    }
+}
+
+// Our frontend. User will go through NGINX to get to our app
+resource "docker_image" "nginx-frontend" {
+  name         = "nginx-frontend"
   keep_locally = false
+  build {
+      path = "./nginx-proxy/"
+      tag = ["nginx-frontend:latest"]
+  }
+}
+
+resource "docker_container" "node-app" {
+    name = "node-app"
+    image = docker_image.node-app.latest
+    must_run = true
+    
+    // This should only be accessible by reverse proxy. Bind to localhost only
+    ports {
+        internal = 3000
+        ip = "127.0.0.1"
+    }
+
+    networks_advanced {
+        name = "terraform-net"
+        aliases = ["nodeapp"]
+    }
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx-frontend.latest
   name  = "example-terraform-docker"
+  
   ports {
     internal = 80
     external = 8080
   }
+
+  networks_advanced {
+        name = "terraform-net"
+        aliases = ["frontend"]
+    }
 }
